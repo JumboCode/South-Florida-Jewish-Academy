5766053563efef1f45d87a94f185f98cd78052be
Lawrence Chan <chanlawrencet@gmail.com>
2019-09-16 23:01:24 -0400

    added hello world api on flask

---
+++ api/run.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        listOfNums = []
+        for i in range(0, 10):
+            listOfNums.append(i)
+
+        return {'list': listOfNums }
+
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
