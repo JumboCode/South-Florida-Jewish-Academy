560b5f8da23999224e2151485974ee24a0b60f18
Lawrence Chan <chanlawrencet@gmail.com>
2019-09-29 11:23:38 -0400

    travis and rename

---
+++ sfja/src/components/home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+class Home extends React.Component{
+    render() {
+        return (
+            <div>
+                Hi! Welcome to SFJA.
+            </div>
+        );
+    }
+}
+
+export default Home
