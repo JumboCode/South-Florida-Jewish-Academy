2e08d1f6f13b44813825cc63ee5c31cdd4b6ffed
BadCubozoa <thespeedygenius@gmail.com>
2019-10-14 18:41:34 -0400

    #44 added comments for parentsDOM functions

--- api/database/parentsDOM.py
+++ api/database/parentsDOM.py
@@ -4,3 +4,8 @@ from flask_pymongo import PyMongo
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/SFJA"
 mongo = PyMongo(app)
+
+# update info
+# given all info make new parent
+# list student(s) associated with parent
+# if student_id is changed, change here also
