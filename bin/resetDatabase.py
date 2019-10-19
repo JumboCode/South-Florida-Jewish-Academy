b72011adb13368619d7e04cee82fb7e66de836d5
Janny Huang <jannyhuang12@gmail.com>
2019-10-19 15:01:29 -0400

    change users to students

--- bin/resetDatabase.py
+++ bin/resetDatabase.py
@@ -1,17 +1,24 @@
 from pymongo import MongoClient
 
 
-def makeUsers(db):
-    users = db.users
+def makeStudents(db):
+    students = db.students
     print('Made users')
+    # def genRandomForms(n):
+    #     from random import randrange
+    #     forms = {}
+    #     for i in range(n):
+    #         forms['form' + str(i)]
     for i in range(0, 10):
         initData = {
             'first_name': 'first' + str(i),
             'middle_name': 'middle' + str(i),
             'last_name': 'last' + str(i),
-            'DOB': str(i)*4 + str(i)*2 + str(i)*2,
-            'parent_ids': ["parent_1_" + str(1), "parent_2_" + str(i)],
+            'student_id': i,
+            'DOB': str(i)*4 + "-" + str(i)*2 + "-" + str(i)*2,
+            'parent_ids': ["parent_1_" + str(1), "parent_2_" + str(i)]
             'email': 'user' + str(i) + '@FloridaJewishAcademy.org'
+            'forms': 
         }
         result = users.insert_one(initData)
         print('Inserted ', result.inserted_id)
