f76b49c6698565a20f1f10128f047b309d46736e
Anthony Tranduc <anthonytranduc@gmail.com>
2019-10-20 17:13:21 -0400

    #44 Started writing API test case

--- api/database/studentsDOM.py
+++ api/database/studentsDOM.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/SFJA"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/sfja"
 mongo = PyMongo(app)
 
 # Given all info, make new student
@@ -11,12 +11,13 @@ mongo = PyMongo(app)
 # Get forms from student
 
 def updateInfo(id, key, update):
-    writeR = mongo.db.students.update({'id': str(id)}, {key: update})
+    writeR = mongo.db.students.update({'student_id': str(id)}, {key: update})
     # TODO: Inform of update.
 
 def getInfo(id, key):
-    content = mongo.db.students.find({'id': str(id)})
-    return content[key]
+    contents = list(mongo.db.students.find({'student_id':id}))
+    for content in contents:
+        return(content['basic_info'][key])
 
 def getStudentForm(id, formNum):
     content = mongo.db.students.find({'id': str(id)})
--- api/tests.py
+++ api/tests.py
@@ -1,6 +1,14 @@
+from database.studentsDOM import getInfo
+
+def testStudentGetInfo():
+	expected = 'first0'
+	actual = getInfo(0, 'first_name')
+	return expected == actual
+
 
 
 def main():
+	print('StudentDOM getInfo: ' + str(testStudentGetInfo()))
 	return 1
 
 
