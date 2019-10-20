0d1664477b2611382e30110c72f4da9ccbe36155
Anthony Tranduc <anthonytranduc@gmail.com>
2019-10-20 18:10:01 -0400

    4 StudentDOM

--- api/database/studentsDOM.py
+++ api/database/studentsDOM.py
@@ -10,14 +10,16 @@ mongo = PyMongo(app)
 # Give id, string, value, update student info
 # Get forms from student
 
-def updateInfo(id, key, update):
-    writeR = mongo.db.students.update({'student_id': str(id)}, {key: update})
-    # TODO: Inform of update.
+def putInfo(id, key, update):
+    writeR = dict(mongo.db.students.update({'student_id': id}, {'$set': {'basic_info.' + str(key): update}}))
+    if writeR['nModified'] > 0:
+        return True
+    return False
 
 def getInfo(id, key):
-    contents = list(mongo.db.students.find({'student_id':id}))
+    contents = list(mongo.db.students.find({'student_id': id}))
     for content in contents:
-        return(content['basic_info'][key])
+        return content['basic_info'][key]
 
 def getStudentForm(id, formNum):
     content = mongo.db.students.find({'id': str(id)})
