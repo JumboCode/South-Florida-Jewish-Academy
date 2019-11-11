3b24478d6f9dd288ddc6a138c309bed97268ded7
Anthony Tranduc <anthonytranduc@gmail.com>
2019-11-11 01:27:03 -0500

    #53 Finished studentsDOM/formsDOM and test

--- api/database/studentsDOM.py
+++ api/database/studentsDOM.py
@@ -4,26 +4,29 @@ import os
 
 app = Flask(__name__)
 MONGO_URL = os.environ.get('MONGODB_URI')
-app.config["MONGO_URI"] = MONGO_URL
+app.config["MONGO_URI"] = "mongodb://localhost:27017/sfja"
 mongo = PyMongo(app)
 
 # Creates a new student in the database. Takes pre-made
 # basicInfo and formIds dictionaries.
-def postStudent(id, basicInfo, formIds):
+def createStudent(id, basicInfo, formIds):
     initData = {
                 'student_id': id,
                 'basic_info': basicInfo,
                 'form_ids': formIds,
-            }
+                }
     result = mongo.db.students.insert_one(initData)
     return result.inserted_id
+
+# Deletes student.
+def deleteStudent(id):
+    results = mongo.db.students.delete_one({'student_id': id})
+    return results
     
 # Updates student basic info.
-def putInfo(id, key, update):
+def updateInfo(id, key, update):
     writeR = dict(mongo.db.students.update({'student_id': id}, {'$set': {'basic_info.' + str(key): update}}))
-    if writeR['nModified'] > 0:
-        return True
-    return False
+    return writeR['nModified'] > 0
 
 # Gets student basic info.
 def getInfo(id, key):
@@ -35,17 +38,22 @@ def getInfo(id, key):
 def getForm(id, formNum):
     contents = list(mongo.db.students.find({'student_id': id}))
     for content in contents:
-        return content['form_ids'][str(formNum)]
-
+        if str(formNum) in content['form_ids']:
+            return content['form_ids'][str(formNum)]
+        else:
+            return None
 # Add new form.
-def postForm(id, formNum, formId):
+def addForm(id, formNum, formId):
     writeR = dict(mongo.db.students.update({'student_id': id}, {'$set': {'form_ids.' + str(formNum): str(formId)}}))
-    if writeR['nModified'] > 0:
-        return True
-    return False
+    return writeR['nModified'] > 0
 
+# Removes form from student data.
+def removeForm(id, formNum):
+    writeR = dict(mongo.db.students.update({'student_id': id}, {'$unset': {'form_ids.' + str(formNum): ''}}))
+    return writeR['nModified'] > 0
+
+# Gets a form.
 def getStudentForm(id, formNum):
-    content = mongo.db.students.find({'id': str(id)})
+    content = mongo.db.students.find({'student_id': id})
     forms = dict(content['form'])
     return forms[formNum]
-
--- api/tests.py
+++ api/tests.py
@@ -1,28 +1,30 @@
 from database import studentsDOM
+from database import formsDOM
 
+# ---------------- STUDENTS -----------------------
 def testStudentGetInfo():
 	expected = 'first0'
 	actual = studentsDOM.getInfo(0, 'first_name')
 	return expected == actual
 
-def testStudentPutInfo():
+def testStudentUpdateInfo():
 	expected = 'newlast0'
-	studentsDOM.putInfo(0, 'last_name', 'newlast0')
+	studentsDOM.updateInfo(0, 'last_name', 'newlast0')
 	actual = studentsDOM.getInfo(0, 'last_name')
 	return expected == actual
 
 def testStudentGetForm():
-	expected = '32'
+	expected = ['320', '321']
 	actual = studentsDOM.getForm(3, 0)
 	return expected == actual
 
-def testStudentPostForm():
+def testStudentAddForm():
 	expected = '987'
-	studentsDOM.postForm(3, 16, 987)
+	studentsDOM.addForm(3, 16, 987)
 	actual = studentsDOM.getForm(3, 16)
 	return expected == actual
 
-def testStudentPostStudent():
+def testStudentCreateStudent():
 	newBasicInfo = {
         		'first_name': 'newfirst',
         		'middle_name': 'newmiddle',
@@ -32,16 +34,68 @@ def testStudentPostStudent():
         		'email': 'user999@FloridaJewishAcademy.org'
         		}
 	newFormIds = {'123': '123'}
-	studentsDOM.postStudent(500, newBasicInfo, newFormIds)
+	studentsDOM.createStudent(500, newBasicInfo, newFormIds)
 	return True
+
+def testStudentDeleteStudent():
+	expected = None
+	studentsDOM.deleteStudent(8)
+	actual = studentsDOM.getForm(8, 2)
+	return expected == actual
+
+def testStudentRemoveForm():
+	expected = None
+	studentsDOM.removeForm(4, 4)
+	actual = studentsDOM.getForm(4, 4)
+	return expected == actual
+
+
+# ---------------- FORMS -----------------------
+def testFormCreateForm():
+	newData = {'0': 1000}
+	formsDOM.createForm('1000', '1212-12-12', True, 123, 0.22, newData)
+	actual = formsDOM.getFormData('1000')
+	return newData == actual
+
+def testFormGetInfo():
+	expected = True
+	actual = formsDOM.getInfo('000', 'required')
+	return expected == actual
+
+def testFormGetFormData():
+	expected = {'0': 0, '1': 1, '2': 0, '3': 1}
+	actual = formsDOM.getFormData('000')
+	return expected == actual
+
+def testFormDeleteForm():
+	expected = None
+	formsDOM.deleteForm('020')
+	actual = formsDOM.getFormData('020')
+	return expected == actual
+
+def testFormUpdateFormData():
+	expected = {'0': 0, '1': 1, '2': 0, '3': 1, '123': 321}
+	formsDOM.updateFormData('010', 123, 321)
+	actual = formsDOM.getFormData('010')
+	return actual == expected
+
 
 def main():
 	print('TEST CASES')
-	print('StudentDOM getInfo: ' + str(testStudentGetInfo()))
-	print('StudentDOM putInfo: ' + str(testStudentPutInfo()))
-	print('StudentDOM getForm: ' + str(testStudentGetForm()))
-	print('StudentDOM postForm: ' + str(testStudentPostForm()))
-	print('StudentDOM postForm: ' + str(testStudentPostStudent()))
+	print('-----------------STUDENTS-----------------')
+	print('studentsDOM getInfo: ' + str(testStudentGetInfo()))
+	print('studentsDOM updateInfo: ' + str(testStudentUpdateInfo()))
+	print('studentsDOM getForm: ' + str(testStudentGetForm()))
+	print('studentsDOM addForm: ' + str(testStudentAddForm()))
+	print('studentsDOM createStudent: ' + str(testStudentCreateStudent()))
+	print('studentsDOM deleteStudent: ' + str(testStudentDeleteStudent()))
+	print('studentsDOM removeForm: ' + str(testStudentRemoveForm()))
+	print('-------------------FORMS-------------------')
+	print('formsDOM createForm: ' + str(testFormCreateForm()))
+	print('formsDOM getInfo: ' + str(testFormGetInfo()))
+	print('formsDOM getFormData: ' + str(testFormGetFormData()))
+	print('formsDOM deleteForm: ' + str(testFormDeleteForm()))
+	print('formsDOM updateForm: ' + str(testFormUpdateFormData()))
 	return 1
 
 
