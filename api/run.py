f450b20f4eaa8a777334efdc7ff5b3b7c22e4786
Lawrence Chan <chanlawrencet@gmail.com>
2019-11-11 17:29:28 -0500

    run and backend

--- api/run.py
+++ api/run.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_cors import CORS
 from flask_restful import Resource, Api
 from database import testDB, studentsDOM
+from flask import jsonify
+import subprocess
 # from bin import resetDatabase
 
 app = Flask(__name__)
@@ -9,6 +11,11 @@ CORS(app)
 api = Api(app)
 
 # look I'm a comment
+
+@app.route('/resetDatabase', methods=['GET', 'POST'])
+def resetDatabase():
+    subprocess.call('python3 ../bin/resetDatabase.py', shell=True)
+    return jsonify({'done': True})
 
 @app.route('/', methods = ['GET', 'POST'])
 def HelloWorld():
