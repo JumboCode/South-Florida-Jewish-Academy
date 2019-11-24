70b405be599386782550c2bec4dc81dfe753bfea
Max Ramer <maxramer@Maxs-MacBook-Pro-2.local>
2019-11-24 16:30:22 -0500

    studentDashboard component edit

--- sfja/src/StudentDashboard.js
+++ sfja/src/StudentDashboard.js
@@ -3,28 +3,13 @@ import React from 'react';
 // eslint-disable-next-line require-jsdoc
 class StudentDashboard extends React.Component {
   // eslint-disable-next-line require-jsdoc
-    componentDidMount() {
-        fetch("http://www.mocky.io/v2/5dd1c979320000600006fb0b")
-          .then(res => res.json())
-          .then(
-            (result) => {
-              this.setState({
-                isLoaded: true,
-                items: result.items
-              });
-            },
-            // Note: it's important to handle errors here
-            // instead of a catch() block so that we don't swallow
-            // exceptions from actual bugs in components.
-            (error) => {
-              this.setState({
-                isLoaded: true,
-                error
-              });
-            }
-          )
-      }
 
+
+    componentDidMount() {
+    fetch('http://www.mocky.io/v2/5dd1c979320000600006fb0b')
+      .then(response => response.json())
+      .then(data => this.setState({ data }));
+  }
 
   render() {
 
