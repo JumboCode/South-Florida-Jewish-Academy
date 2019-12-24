a0149e6349a27c1ad6e2f183765d2b4d8cd3daea
Lawrence Chan <chanlawrencet@gmail.com>
2019-12-23 22:30:58 -0500

    Linting files

--- sfja/src/Form.js
+++ sfja/src/Form.js
@@ -1,35 +1,39 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 
 // eslint-disable-next-line require-jsdoc
 class Form extends React.Component {
   // eslint-disable-next-line require-jsdoc
-  postKey = () => {
-    const key = this.props.match.params.key;
+  postKey = (key) => {
     const body = {
-      key: key
+      key: key,
     };
-    fetch('http://127.0.0.1:5000/checkKey', {method:'POST', body:JSON.stringify(body),
-        headers: {
-            'Content-Type': 'application/json'
-        }})
-    .then(response => response.status) 
-    
-    return true
-  }; 
+    fetch('http://127.0.0.1:5000/checkKey', {method: 'POST', body: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json',
+      }})
+        .then((response) => response.status);
+
+    return true;
+  };
 
 
+  // eslint-disable-next-line require-jsdoc
   render() {
-  	let key = this.props.match.params.key;
+    const key = this.props.match.params.key;
 
     return (
       <div>
       key (testing purposes): {key}
-      <br></br>
-      <button onClick={this.postKey}> CHECK </button>
+        <br/>
+        <button onClick={this.postKey(key)}> CHECK </button>
       </div>
     );
   }
 }
+
+Form.propTypes = {
+  match: PropTypes.any,
+};
 
 export default Form;
