79da5dd5681df71e5c295231e4d5c1908b545e23
trish234 <trisha.cox@gmail.com>
2019-12-26 18:24:14 -0800

    Finishing touches

--- sfja/src/Header.js
+++ sfja/src/Header.js
@@ -6,6 +6,8 @@ import {ReactComponent as StudentsIcon} from './VectorsForNavbar/Students.svg';
 import {ReactComponent as HomeIcon} from './VectorsForNavbar/Home.svg';
 import {ReactComponent as EmailIcon} from './VectorsForNavbar/Email.svg';
 import {ReactComponent as LogoutIcon} from './VectorsForNavbar/Logout.svg';
+import topLogo from "./CircleLogo.png";
+
 
 var chosen = document.createAttribute('checked'); 
 chosen.value = "false";   
@@ -16,18 +18,20 @@ class Header extends React.Component {
       const {setTab, selectedTab} = this.props;
       return(
         <div>
+          <img id="topLogo" src={topLogo}></img>
           <div id="toptitle">
-          {/* logo would go here */}
           South Florida Jewish Academy <br></br><div id="topsubtitle">Admissions Page</div>
           </div>
           <div className="navbar">
             <HomeIcon></HomeIcon>
-            <div onClick={() => setTab('dashboard')}> Dashboard
+            <div onClick={() => setTab('dashboard')}>Dashboard
              <div
                  style={{
                   backgroundColor: selectedTab == 'dashboard' ? '#0068af' : null,
-                  width: 150,
-                  height: 10
+                  position: "absolute",
+                  marginTop: 19,
+                  width: 140,
+                  height: 7
                 }} 
               > </div>
             </div>
@@ -36,35 +40,43 @@ class Header extends React.Component {
             <div
                  style={{
                   backgroundColor: selectedTab == 'students' ? '#0068af' : null,
-                  width: 150,
-                  height: 10
+                  position: "absolute",
+                  marginTop: 19,
+                  width: 120,
+                  height: 7
                 }}></div>
             </a>
             <UploadIcon></UploadIcon>
-            <a onClick={() => setTab('upload')}> Upload Forms
+            <a onClick={() => setTab('upload')}>Upload Forms
             <div
                  style={{
                   backgroundColor: selectedTab == 'upload' ? '#0068af' : null,
-                  width: 150,
-                  height: 10
+                  position: "absolute",
+                  marginTop: 19,
+                  width: 170,
+                  height: 7
                 }}></div>
             </a>
             <EmailIcon></EmailIcon>
-            <a onClick={() => setTab('email')}> Email
+            <a onClick={() => setTab('email')}>Email
             <div
                  style={{
                   backgroundColor: selectedTab == 'email' ? '#0068af' : null,
-                  width: 150,
-                  height: 10
+                  position: "absolute",
+                  marginTop: 19,
+                  width: 90,
+                  height: 7
                 }}></div>
             </a>
             <LogoutIcon></LogoutIcon>
-            <a onClick={() => setTab('logout')}> Logout
+            <a onClick={() => setTab('logout')}>Logout
             <div
                  style={{
                   backgroundColor: selectedTab == 'logout' ? '#0068af' : null,
-                  width: 150,
-                  height: 10
+                  position: "absolute",
+                  marginTop: 19,
+                  width: 100,
+                  height: 7
                 }}></div>
             </a>
           </div>
