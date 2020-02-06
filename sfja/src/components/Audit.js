195fce5a4db386b088151501149ef01e5c25d669
BadCubozoa <thespeedygenius@gmail.com>
2020-02-05 20:06:51 -0500

    starter for audit

---
+++ sfja/src/components/Audit.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//hello
+class Audit extends React.Component{
+
+
+    render(){
+        return(
+            <div>U r in audit</div>
+        )
+    }
+}
+
+export default Audit;
