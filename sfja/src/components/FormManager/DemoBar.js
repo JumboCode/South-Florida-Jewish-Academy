f5f6b707f218a8e73f6cc93f69da7c6e63c80d24
trish234 <trisha.cox@gmail.com>
2020-02-17 20:10:45 -0500

    Get rid of preview

--- sfja/src/components/FormManager/DemoBar.js
+++ sfja/src/components/FormManager/DemoBar.js
@@ -1,19 +1,5 @@
 import React from 'react';
 import store from './FormBuilder/stores/store';
-import ReactFormGenerator from './FormBuilder/form';
-
-const answers = {};
-// const answers = {
-//   'dropdown_38716F53-51AA-4A53-9A9B-367603D82548': 'd2',
-//   'checkboxes_8D6BDC45-76A3-4157-9D62-94B6B24BB833': [
-//     'checkboxes_option_8657F4A6-AA5A-41E2-A44A-3E4F43BFC4A6',
-//     'checkboxes_option_1D674F07-9E9F-4143-9D9C-D002B29BA9E4',
-//   ],
-//   'radio_buttons_F79ACC6B-7EBA-429E-870C-124F4F0DA90B': [
-//     'radiobuttons_option_553B2710-AD7C-46B4-9F47-B2BD5942E0C7',
-//   ],
-//   'rating_3B3491B3-71AC-4A68-AB8C-A2B5009346CB': 4,
-// };
 
 // eslint-disable-next-line require-jsdoc
 export default class Demobar extends React.Component {
@@ -65,43 +51,12 @@ export default class Demobar extends React.Component {
 
   // eslint-disable-next-line require-jsdoc
   render() {
-    let modalClass = 'modal';
-    if (this.state.previewVisible) {
-      modalClass += ' show';
-    }
 
     return (
       <div className="clearfix" style={{margin: '10px', width: '70%'}}>
         <h4 className="pull-left">Preview</h4>
         <button className="btn btn-primary pull-right" style={{marginRight:
-          '10px'}} onClick={this.showPreview.bind(this)}>Preview Form</button>
-
-        { this.state.previewVisible &&
-          <div className={modalClass}>
-            <div className="modal-dialog modal-lg">
-              <div className="modal-content">
-                <ReactFormGenerator
-                  download_path=""
-                  back_action="/"
-                  back_name="Back"
-                  answer_data={answers}
-                  action_name="Save"
-                  form_action="/api/form"
-                  form_method="POST"
-                  // skip_validations={true}
-                  onSubmit={this._onSubmit}
-                  // variables={this.props.variables}
-                  data={this.state.data} />
-
-                <div className="modal-footer">
-                  <button type="button" className="btn btn-default"
-                    data-dismiss="modal" onClick={this.closePreview.bind(this)}>
-                    Close</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        }
+          '10px'}} onClick={this._onSubmit}>Submit Form</button>
       </div>
     );
   }
