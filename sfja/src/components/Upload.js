1b8005f4ad3ed50d43c1f7b426744345026e140d
trish234 <trisha.cox@gmail.com>
2020-02-23 20:28:26 -0500

    Skeleton of Upload.js

--- sfja/src/components/Upload.js
+++ sfja/src/components/Upload.js
@@ -5,12 +5,32 @@ import FormManager from './FormManager/FormManager';
 
 // eslint-disable-next-line require-jsdoc
 class Upload extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      createForm: false,
+      currentForm: false,
+      formsList: [],
+    };
+  }
+//TODO function to load in all forms to formsList
+
   // eslint-disable-next-line require-jsdoc
   render() {
+    const {createForm} = this.state;
+    const {currentForm} = this.state;
     return (
       <div>
         <Header currTab='upload'/>
-        <FormManager/>
+        {createForm ? <FormManager/>: 
+          <div>
+            <button onClick= {() => this.setState({createForm: true})}> Add Form </button>
+            <ul>
+              <div> I'm a form </div>
+            </ul>
+          </div>
+        }
+
       </div>
     );
   }
