161f6fcb615c4d4549754249f468035cc247a768
Anthony Tranduc <anthonytranduc@gmail.com>
2020-04-18 18:59:21 -0400

    #123 deleted unsued file and linting

--- sfja/src/components/ParentDash/StudentDash.js
+++ sfja/src/components/ParentDash/StudentDash.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ReactFormGenerator from '../FormManager/FormBuilder/form';
-import { makeStyles } from '@material-ui/core/styles';
+// eslint-disable-next-line max-len
+import ReactFormGenerator from '../FormManager/BlankFormBuilder/FormBuilder/form';
+import {makeStyles} from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import TableContainer from '@material-ui/core/TableContainer';
 import Table from '@material-ui/core/Table';
@@ -9,7 +10,6 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
-import {Select, InputLabel, MenuItem} from '@material-ui/core';
 import apiUrl from '../../utils/Env';
 
 const useStyles = makeStyles({
@@ -42,20 +42,21 @@ class StudentDash extends React.Component {
     this.refreshStudentForms();
   }
 
-    // eslint-disable-next-line require-jsdoc
-    refreshStudentForms() {
-      fetch(apiUrl() + '/getStudentForms', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({student_id: this.state.studentId}),
-      }).then((res) => res.json())
+  // eslint-disable-next-line require-jsdoc
+  refreshStudentForms() {
+    fetch(apiUrl() + '/getStudentForms', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({student_id: this.state.studentId}),
+    }).then((res) => res.json())
         .then((data) => {
-          this.setState({formData : data.form_data});
+          this.setState({formData: data.form_data});
           console.log(data);
         }).catch(console.log);
-    }
+  }
 
-  refreshFormData(formId){
+  // eslint-disable-next-line require-jsdoc
+  refreshFormData(formId) {
     fetch('http://127.0.0.1:5000/getForm', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
@@ -64,78 +65,83 @@ class StudentDash extends React.Component {
     }).then((res) => res.json())
         .then((data) => {
           this.setState({blankFormData: data.blank_form_data,
-                        formFilledData: data.form_data});
+            formFilledData: data.form_data});
           console.log(data);
         }).catch(console.log);
   }
 
+  // eslint-disable-next-line no-invalid-this
   isSelected = (formId) => this.state.selected == formId
 
   handleOnClick = (event, formId) => {
+    // eslint-disable-next-line no-invalid-this
     this.setState({selected: formId});
-    this.refreshFormData(formId)
+    // eslint-disable-next-line no-invalid-this
+    this.refreshFormData(formId);
   }
 
-  handleSubmit(formId, answerData){
+  // eslint-disable-next-line require-jsdoc
+  handleSubmit(formId, answerData) {
     fetch('http://127.0.0.1:5000/submitForm', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       // eslint-disable-next-line react/prop-types
       body: JSON.stringify({form_id: formId,
-                            answer_data: answerData}),
-    }).then(response => response);
+        answer_data: answerData}),
+    }).then((response) => response);
   }
 
   // eslint-disable-next-line require-jsdoc
   render() {
-    const {studentId, formData, selected, formFilledData, blankFormData, showForm} = this.state;
+    // eslint-disable-next-line max-len
+    const {studentId, formData, selected, formFilledData, blankFormData} = this.state;
 
     if (studentId !== this.props.match.params.studentId) {
       this.refreshStudentForms();
     }
-    console.log(blankFormData)
+    console.log(blankFormData);
     return (
       <div>
         <h1>HELLO STUDENT {studentId}</h1>
         <TableContainer component={Paper}>
           <Table className={useStyles.table} aria-label="form table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Form Name</TableCell>
-              <TableCell align="right">Last Updated</TableCell>
-              <TableCell align="right">Last Viewed</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-          {formData.map((form) => (
-            <TableRow 
-              key={form.form_id}
-              onClick={(event) => this.handleOnClick(event, form.form_id)}
-              selected={this.isSelected(form.form_id)}
-            >
-              <TableCell component="th" scope="row">
-                {form.form_name}
-              </TableCell>
-              <TableCell align="right">{form.last_updated}</TableCell>
-              <TableCell align="right">{form.last_viewed}</TableCell>
-            </TableRow>
-          ))}
-          </TableBody>
+            <TableHead>
+              <TableRow>
+                <TableCell>Form Name</TableCell>
+                <TableCell align="right">Last Updated</TableCell>
+                <TableCell align="right">Last Viewed</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {formData.map((form) => (
+                <TableRow
+                  key={form.form_id}
+                  onClick={(event) => this.handleOnClick(event, form.form_id)}
+                  selected={this.isSelected(form.form_id)}
+                >
+                  <TableCell component="th" scope="row">
+                    {form.form_name}
+                  </TableCell>
+                  <TableCell align="right">{form.last_updated}</TableCell>
+                  <TableCell align="right">{form.last_viewed}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
           </Table>
         </TableContainer>
-        {console.log("FORM GENERATOR")} 
+        {console.log('FORM GENERATOR')}
         {console.log(typeof blankFormData)}
-        {blankFormData !== null      
-          ? <ReactFormGenerator
-              onSubmit={this.handleSubmit}
-              form_action=""
-              form_method="POST"
-              task_id={12}
-              answer_data={formFilledData}
-              data={blankFormData} // Question data
-              form_id={selected}
-            />
-          : <h1>Please select a form.</h1>
+        {blankFormData !== null ?
+          <ReactFormGenerator
+            onSubmit={this.handleSubmit}
+            form_action=""
+            form_method="POST"
+            task_id={12}
+            answer_data={formFilledData}
+            data={blankFormData} // Question data
+            form_id={selected}
+          /> :
+          <h1>Please select a form.</h1>
         }
       </div>
     );
