49ab20acf0a3d7f8d4a760446b24088d80a7ee70
Jacqueline Chin <chin.jacqueline1@gmail.com>
2020-04-24 17:21:13 -0400

    #122

---
+++ sfja/src/components/StudentProfile/DocumentUpload.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Main from './Main'
+class DocumentUpload extends React.Component {
+
+  render() {
+    return (
+      <Main/>
+    );
+  }
+}
+
+export default DocumentUpload;
