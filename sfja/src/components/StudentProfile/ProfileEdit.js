0b8cd00c6ee97aefff430b9cc90b87507e099918
Jacqueline Chin <chin.jacqueline1@gmail.com>
2020-05-08 22:27:01 -0400

    #122: put in ProfileEdit did not merge from master properly

--- sfja/src/components/StudentProfile/ProfileEdit.js
+++ sfja/src/components/StudentProfile/ProfileEdit.js
@@ -1,35 +1,158 @@
 import React from 'react';
+import {Button, TextField} from '@material-ui/core';
+import {
+  KeyboardDatePicker,
+  MuiPickersUtilsProvider,
+} from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
+import {instanceOf} from 'prop-types';
+import {Cookies, withCookies} from 'react-cookie';
+import apiUrl from '../../utils/Env';
+import SnackBarMessage from '../../utils/SnackBarMessage';
+
+const textSize = {
+  style: {fontSize: 15},
+  autoComplete: 'new-password',
+  form: {
+    autoComplete: 'off',
+  },
+};
+
+const textWidth = {
+  padding: 10,
+  marginBottom: 10,
+};
 
 // eslint-disable-next-line require-jsdoc
 class ProfileEdit extends React.Component {
+  static propTypes = {
+    cookies: instanceOf(Cookies).isRequired,
+  };
   // eslint-disable-next-line require-jsdoc
   constructor(props) {
     super(props);
     // eslint-disable-next-line react/prop-types
     const {basicInfo} = this.props;
-    console.log(basicInfo);
+    const oldBasicInfo = JSON.parse(JSON.stringify(basicInfo));
     this.state = {
-      // eslint-disable-next-line react/prop-types
-      oldFirstName: basicInfo.first_name,
-      // eslint-disable-next-line react/prop-types
-      newFirstName: basicInfo.first_name,
+      oldBasicInfo: oldBasicInfo,
+      basicInfo: basicInfo,
+      openSuccessMessage: false,
+      disableButton: true,
     };
   }
 
   // eslint-disable-next-line require-jsdoc
+  sendUpdate() {
+    const {cookies} = this.props;
+    const {basicInfo} = this.state;
+    this.setState({
+      disableButton: true,
+    });
+    const body = {
+      basicInfo: basicInfo,
+      id: basicInfo['_id'],
+    };
+
+    fetch(apiUrl() + '/studentProfileUpdate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${cookies.get('token')}`,
+      },
+      body: JSON.stringify(body),
+    }).then((x) => {
+      if (x.status === 200) {
+        this.setState({
+          oldBasicInfo: JSON.parse(JSON.stringify(basicInfo)),
+          openSuccessMessage: true,
+        });
+      }
+    });
+  }
+
+  // eslint-disable-next-line require-jsdoc
+  updateValue(key, value) {
+    const {basicInfo} = this.state;
+    basicInfo[key] = value;
+    this.setState({
+      basicInfo: basicInfo,
+      disableButton: !this.isDifference(),
+    });
+  }
+  // eslint-disable-next-line require-jsdoc
+  isDifference() {
+    const {basicInfo, oldBasicInfo} = this.state;
+    let difference = false;
+    Object.keys(oldBasicInfo).forEach((key) => {
+      if (oldBasicInfo[key] !== basicInfo[key]) {
+        difference = true;
+      }
+    });
+    return difference;
+  }
+
+  // eslint-disable-next-line require-jsdoc
+  titleFormatter(str) {
+    return str[0].toUpperCase() + str.replace('_', ' ').substring(1);
+  }
+  // eslint-disable-next-line require-jsdoc
   render() {
-    const {newFirstName, oldFirstName} = this.state;
+    const {basicInfo, openSuccessMessage, disableButton} = this.state;
     return (
-      <div>
-                Edit info:
-        <div>
+      <div style={{padding: 20}}>
+        <div style={{display: 'flex', flexWrap: 'wrap'}}>
           {/* eslint-disable-next-line max-len */}
-          <input type="text" name="name" value={newFirstName} onChange={(theChange) => this.setState({newFirstName: theChange.target.value})}/>
-          {newFirstName !== oldFirstName ? <div>changed</div> : <div/>}
+          {Object.keys(basicInfo).filter((key) => (key !== '_id' && key !== 'DOB')).map((key) => (
+            <div key={key}>
+              {/* eslint-disable-next-line max-len */}
+              <TextField onChange={(event) => this.updateValue(key, event.target.value)} value={basicInfo[key]} style={textWidth} inputProps={textSize} variant='outlined' id="standard-basic" label={this.titleFormatter(key)} required={true}/>
+            </div>
+          ))}
+          <MuiPickersUtilsProvider utils={DateFnsUtils} >
+            <KeyboardDatePicker
+              error={basicInfo.DOB === null}
+              required={true}
+              inputProps={textSize}
+              style={{marginLeft: 10, width: 175}}
+              disableToolbar
+              variant="inline"
+              format="MM/dd/yyyy"
+              margin="normal"
+              id="date-picker-inline"
+              label="Birthday"
+              value={basicInfo.DOB}
+              onChange={
+                (event) => this.updateValue(
+                    'DOB',
+                    event ? event.toLocaleDateString() : null)
+              }
+              KeyboardButtonProps={{
+                'aria-label': 'change date',
+              }}
+            />
+          </MuiPickersUtilsProvider>
         </div>
+        {/* eslint-disable-next-line max-len */}
+        <div style={{display: 'flex', flexDirection: 'row-reverse'}}>
+          <Button
+            variant='contained'
+            size='large'
+            onClick={this.sendUpdate.bind(this)}
+            disabled={disableButton}>
+            Update
+          </Button>
+        </div>
+
+        <SnackBarMessage
+          open={openSuccessMessage}
+          closeSnackbar={() => this.setState({openSuccessMessage: false})}
+          severity='success'
+          message='Successfully Updated.'
+        />
       </div>
     );
   }
 }
 
-export default ProfileEdit;
+export default withCookies(ProfileEdit);
