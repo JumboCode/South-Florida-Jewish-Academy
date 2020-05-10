98419e9cc404838611d6273bc340df7900ef2974
Lawrence Chan <chanlawrencet@gmail.com>
2020-05-10 00:38:38 -0400

    #196 Parent Dash

--- sfja/src/components/ParentDash/ParentDash.js
+++ sfja/src/components/ParentDash/ParentDash.js
@@ -1,12 +1,68 @@
 import React from 'react';
+import apiUrl from '../../utils/Env';
+import Paper from "@material-ui/core/Paper";
+import TableContainer from "@material-ui/core/TableContainer";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import TableBody from "@material-ui/core/TableBody";
+import Complete from "../../utils/Complete";
+import Incomplete from "../../utils/Incomplete";
 
 // eslint-disable-next-line require-jsdoc
 class ParentDash extends React.Component {
   // eslint-disable-next-line require-jsdoc
+
+  // eslint-disable-next-line require-jsdoc
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+    };
+  }
+
+  // eslint-disable-next-line require-jsdoc
+  componentDidMount() {
+    fetch(apiUrl() + '/getParentInfo', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      // eslint-disable-next-line react/prop-types
+      body: JSON.stringify({curr_link: this.props.match.params.parentKey}),
+    }).then((res) => res.json())
+        .then((data) => {
+          this.setState({
+            firstName: data.first_name,
+            lastName: data.last_name,
+            email: data.email,
+          });
+        });
+  }
+
+  // eslint-disable-next-line require-jsdoc
   render() {
+    const {firstName, lastName, email} = this.state;
     return (
-      <div>
-        welcome
+      // eslint-disable-next-line max-len
+      <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: 20}}>
+        <Paper elevation={2} style={{padding: 20, minWidth: 650}} >
+          <div style={{paddingBottom: 10, fontSize: 20}}>
+            Welcome to your SFJA form portal!
+            <br/>
+            <br/>
+            Your information
+          </div>
+          <div style={{paddingBottom: 10, fontSize: 15}}>
+            Name: {firstName} {lastName}
+            <br/>
+            Email: {email}
+          </div>
+          <div style={{paddingBottom: 10, fontSize: 15}}>
+            Please on a student tab above to get started.
+          </div>
+        </Paper>
       </div>
     );
   }
