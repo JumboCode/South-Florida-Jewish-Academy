703e4691c3c515f19c43f28ef7650646507f8e2a
Lawrence Chan <chanlawrencet@gmail.com>
2020-05-24 20:52:46 -0400

    #243 auth0 wrapper

---
+++ sfja/src/utils/Auth0Wrapper.js
@@ -0,0 +1,24 @@
+import {useAuth0} from '../react-auth0-spa';
+import React, {useState} from 'react';
+
+
+
+// eslint-disable-next-line require-jsdoc
+export function withAuth0(Component) {
+  return function WrappedComponent(props) {
+    const {getTokenSilently, loading} = useAuth0();
+    const [token, setToken] = useState(null);
+
+    if (loading) {
+      return <div/>;
+    }
+    getTokenSilently().then((token) => {
+      setToken(token);
+    });
+    if (token === null) {
+      return <div/>;
+    } else {
+      return <Component {...props} token={token} />;
+    }
+  };
+};
